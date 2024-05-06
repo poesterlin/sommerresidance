332e5d54ce25a204de8e72ebc608a949ed2e6924
Philip Oesterlin <oesterlin@ugd.ai>
2024-05-06 19:18:30 +0200

    scroll to the top after navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
+	import { afterNavigate } from '$app/navigation';
 	import './styles.css';
+
+	afterNavigate(() => {
+		if (browser) {
+			window.scrollTo(0, 0);
+		}
+	});
 </script>
 
 <slot />
