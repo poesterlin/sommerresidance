d9d232db59e0afed9eeaa55e5f2981c272f02451
Philip Oesterlin <philip.oesterlin@gmail.com>
2024-06-25 14:47:29 +0200

    coming soon tags

--- src/routes/(authorized)/timetable/+page.svelte
+++ src/routes/(authorized)/timetable/+page.svelte
@@ -1,119 +1,42 @@
 <svelte:head><title>Timetable | SommerResiDance</title></svelte:head>
 <h1>Timetable</h1>
-<table>
-	<tbody>
-		<tr><td colspan="3" rowspan="1">Freitag</td></tr>
-		<tr>
-			<td colspan="1" rowspan="1">ab 12:00</td>
-			<td colspan="1" rowspan="1">Anreise</td>
-			<td colspan="1" rowspan="1"></td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">18:00 Uhr</td>
-			<td colspan="1" rowspan="1">Offical Start</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">18:30 Uhr</td>
-			<td colspan="1" rowspan="1">Speed-Dating</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">19:00 Uhr</td>
-			<td colspan="1" rowspan="1">Band 1</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">19:30 Uhr</td>
-			<td colspan="1" rowspan="1">Band 2</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">20:30 Uhr</td>
-			<td colspan="1" rowspan="1">Band 3</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">21:30 Uhr</td>
-			<td colspan="1" rowspan="1">DJ 1</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">23:00 Uhr</td>
-			<td colspan="1" rowspan="1">Spotify</td>
-			<td colspan="1" rowspan="1">Tent</td>
-		</tr>
-	</tbody>
-</table>
-<table>
-	<tbody>
-		<tr><td colspan="3" rowspan="1">Samstag</td></tr>
-		<tr>
-			<td colspan="1" rowspan="1">10:00 Uhr</td>
-			<td colspan="1" rowspan="1">Yoga mit Julia oder nur So?</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">11:00 Uhr - 16:00 Uhr</td>
-			<td colspan="1" rowspan="1">Workshops</td>
-			<td colspan="1" rowspan="1">Festivalgelände</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">13:00 Uhr</td>
-			<td colspan="1" rowspan="1">Dj 2</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">14:30 Uhr</td>
-			<td colspan="1" rowspan="1">Dj 3</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">16:00 Uhr</td>
-			<td colspan="1" rowspan="1">Dj 4</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">17:30 Uhr</td>
-			<td colspan="1" rowspan="1">Dj 5</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">19:00 Uhr</td>
-			<td colspan="1" rowspan="1">Dj 6</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">20:30 Uhr</td>
-			<td colspan="1" rowspan="1">Dj 7</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">22:00 Uhr</td>
-			<td colspan="1" rowspan="1">Dj 8</td>
-			<td colspan="1" rowspan="1">Main Stage</td>
-		</tr>
-		<tr>
-			<td colspan="1" rowspan="1">23:00 Uhr</td>
-			<td colspan="1" rowspan="1">Spotify</td>
-			<td colspan="1" rowspan="1">Tent</td>
-		</tr>
-	</tbody>
-</table>
+
+<p>Coming soon!</p>
 
 <style>
-	table {
-		width: 100%;
-		border-collapse: collapse;
-		margin-bottom: 3rem;
+	h1 {
+		margin-top: 0;
 	}
 
-	td {
-		border: 1px solid currentColor;
-		padding: 0.5rem;
+	p {
+		margin-top: 0;
+		animation: rainbow 5s infinite;
+		font-size: 8em;
+		text-decoration: wavy underline;
+		text-underline-offset: 0.3em;
 	}
 
-	td[colspan='3'] {
-		font-weight: bold;
+	@keyframes rainbow {
+		0% {
+			text-decoration-color: red;
+		}
+		16% {
+			text-decoration-color: orange;
+		}
+		33% {
+			text-decoration-color: yellow;
+		}
+		50% {
+			text-decoration-color: green;
+		}
+		66% {
+			text-decoration-color: blue;
+		}
+		83% {
+			text-decoration-color: indigo;
+		}
+		100% {
+			text-decoration-color: violet;
+		}
 	}
 </style>
--- src/routes/(authorized)/workshops/+page.svelte
+++ src/routes/(authorized)/workshops/+page.svelte
@@ -1,130 +1,43 @@
-<script lang="ts">
-	const workshops = [
-		{
-			header: 'Nageln auf dem Festival - gepflegte Fingernägel als Dosenöffner',
-			content: {
-				expect:
-					'Wer kennt es nicht... Beim Öffnen des Dosenbiers abgerutscht, beim Moshpit langelegt, die Dosenravioli mal wieder ohne Besteck genossen, beim Kettenrauchen den Filter mehrfach ignoriert... Unästhetische Fingernägel sind die Folge! Wer dieser Ursache für das Nichtfinden neuer Freunde, die Ablehnung auf der Luftmatratzenkante, den sofortigen Rauswurf aus dem Festival und die Verachtung des eigenen Selbsts nun ein Schnippchen schlagen will, dem zeigen wir in ein paar einfachen Schritten wie er stattdessen seine Fingernägel zum adäquaten Accessoire aufhübschen kann. Wer nur am Lack schnüffeln will, ist auch herzlich willkommen!',
-				trainer: 'Annika & Dieter',
-				tools: 'gewaschene Hände und ein kühles Getränk',
-				time: '',
-				duration: '30 - 45 Minuten / Einstieg nur am Anfang möglich'
-			}
-		},
-		{
-			header: 'Wie man sich in einem Campingstuhl richtig besäuft und nichts tut!',
-			content: {
-				expect:
-					'Die Elite bekommt Shisha (vlt. auch mehr) zum Rauchen, wird belehrt über ADaG (Advanced Drinking and Gaming) und zum Schluss sind alle sturzbesoffen.',
-				trainer: 'Philip Pumba',
-				tools: 'Campingstuhl und Alkohol',
-				time: 'ab 11:00 Uhr',
-				duration: 'flexibler Ein- und Ausstieg möglich'
-			}
-		},
-		{
-			header: 'SCHOCK DEINE MUDDA',
-			content: {
-				expect:
-					'Verleihe deiner Haut ein temporäres Kunstwerk mit Designer-Tattoos auf Transferfolie. Fühl dich (kurz) cool und schocke deine Mutter. Ach übrigens: Wer sich das Hangover Tribal ins Gesicht tätowieren lässt, bekommt eine Überraschung.',
-				trainer: 'Lea',
-				time: 'geh einfach zu Lea und du bekommst dein Tattoo',
-				duration: 'geh einfach zu Lea und du bekommst dein Tattoo'
-			}
-		},
-		{
-			header: 'Uh Baby, swing dein Teil!',
-			content: {
-				expect: 'Tanzworkshop West Coast Swing',
-				trainer: 'Laura und Peter',
-				tools: 'Bitte als Tanzpaar kommen, Gender-Konstellation egal - hauptsache Spaß am Tanzen',
-				duration: '90 Minuten / Einstieg nur am Anfang möglich'
-			}
-		},
-		{
-			header: 'Bohren und Blasen',
-			content: {
-				expect: 'Wir basteln Flöten aus Karotten. Pflichttermin für alle, die ihre Blockflöte vergessen haben ;)',
-				trainer: 'Tina, Sascha und Fabi',
-				duration: 'flexibler Ein- und Ausstieg möglich'
-			}
-		}
-	];
-</script>
-
 <svelte:head><title>Workshops | SommerResiDance</title></svelte:head>
 
 <h1>Workshops</h1>
 
-<ul>
-	<li>Du kannst irgendwas besonders gut?</li>
-	<li>Du kannst irgendwas gar nicht, möchtest es aber gerne lernen?</li>
-	<li>Du spielst gern ein bestimmtes Spiel?</li>
-	<li>Du machst gern Dinge zusammen mit anderen Menschen?</li>
-</ul>
-
-<p>Sei kreativ :)</p>
-<p>
-	Wende dich mit deiner Workshop Idee an uns - wir freuen uns auf ein breit aufgestelltes Angebot an wilden, witzigen,
-	tollen Aktivitäten, bei denen jeder mitmachen darf.
-</p>
-
-<p>Juhuu, wir haben die besten Freund*innen. Dieses MEGA Angebot an Workshops wird es bei der SOMMERRESIdance geben:</p>
-
-{#each workshops as workshop}
-	<table>
-		<tr>
-			<th colspan="2">
-				{workshop.header}
-			</th>
-		</tr>
-		<tr>
-			<td>Das erwartet dich:</td>
-			<td>
-				{workshop.content.expect}
-			</td>
-		</tr>
-		<tr>
-			<td>Deine Trainer*innen::</td>
-			<td>
-				{workshop.content.trainer}
-			</td>
-		</tr>
-
-		{#if workshop.content.tools}
-			<tr>
-				<td>Bitte mitbringen:</td>
-				<td>
-					{workshop.content.tools}
-				</td>
-			</tr>
-		{/if}
-
-		{#if workshop.content.time}
-			<tr>
-				<td>Zeit & Ort:</td>
-				<td>
-					{workshop.content.time}
-				</td>
-			</tr>
-		{/if}
-
-		<tr>
-			<td>Dauer:</td>
-			<td>
-				{workshop.content.duration}
-			</td>
-		</tr>
-	</table>
-{/each}
+<p>Coming soon!</p>
 
 <style>
-	ul {
-		padding-inline-start: 0;
-		list-style-type: none;
+	h1 {
+		margin-top: 0;
 	}
 
-	li {
-		margin-bottom: 0.3rem;
+	p {
+		margin-top: 0;
+		animation: rainbow 5s infinite;
+		font-size: 8em;
+		text-decoration: wavy underline;
+		text-underline-offset: 0.3em;
+	}
+
+	@keyframes rainbow {
+		0% {
+			text-decoration-color: red;
+		}
+		16% {
+			text-decoration-color: orange;
+		}
+		33% {
+			text-decoration-color: yellow;
+		}
+		50% {
+			text-decoration-color: green;
+		}
+		66% {
+			text-decoration-color: blue;
+		}
+		83% {
+			text-decoration-color: indigo;
+		}
+		100% {
+			text-decoration-color: violet;
+		}
 	}
 </style>
